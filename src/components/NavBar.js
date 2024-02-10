2f43f94d08554da4ac3fda293c84fb00fabfe103
dev-from-cbd <veryfatwombat@gmail.com>
2024-02-11 07:52:33 +1030

    useState, useEffect

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,35 @@
+import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-//import logo from '../assets/img/logo.svg';
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Navbar expand="lg">
       <Container>
         <Navbar.Brand href="#home">
-          <img src={""} alt="logo" />
+          <img src={logo} alt="logo" />
         </Navbar.Brand>
-
         <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className="navbar-toggler-icon">Span</span>
+          <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -18,6 +37,22 @@ export const NavBar = () => {
             <Nav.Link href="#skills">Skills</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
           </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#">
+                <img src={navIcon1} alt="nav Icon #1" />
+              </a>
+              <a href="#">
+                <img src={navIcon2} alt="nav Icon #2" />
+              </a>
+              <a href="#">
+                <img src={navIcon3} alt="nav Icon #3" />
+              </a>
+            </div>
+            <button className="vvd" onClick={() => console.log("connect")}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
