be6d00f90eaa81423f08b69b2e37162719418d2f
dev-from-cbd <veryfatwombat@gmail.com>
2024-03-01 07:05:50 +1030

    comments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
+// Importing logo from the specified path
 import logo from "./logo.svg";
+
+// Importing styles for the App component
 import "./App.css";
+
+// Importing the NavBar component from the specified path
 import { NavBar } from "./components/NavBar";
+
+// Importing the Banner component from the specified path
 import { Banner } from "./components/Banner";
+
+// Importing Bootstrap CSS styles
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// App component function definition
 function App() {
+  // Returning JSX for the App component
   return (
+    // Root div with className "App"
     <div className="App">
+      {/* Rendering the NavBar component */}
       <NavBar />
+      {/* Rendering the Banner component */}
       <Banner />
     </div>
   );
 }
 
+// Exporting the App component as the default export
 export default App;
