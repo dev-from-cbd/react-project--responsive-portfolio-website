1052773351908272e51ae691cbb215f7d56805e8
dev-from-cbd <veryfatwombat@gmail.com>
2024-03-02 09:15:13 +1030

    comments

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,33 @@
+// Importing necessary components from react-bootstrap library
 import { Container, Row, Col } from "react-bootstrap";
+
+// Importing headerImg from the specified path
 import headerImg from "../assets/img/header-img.svg";
+
+// Importing ArrowRightCircle icon from react-bootstrap-icons library
 import { ArrowRightCircle } from "react-bootstrap-icons";
 
+// Defining Banner component as a functional component
 export const Banner = () => {
+  // Returning JSX for the Banner component
   return (
+    // Section element with className "banner" and id "home"
     <section className="banner" id="home">
+      {/* Container component from react-bootstrap */}
       <Container>
+        {/* Row component from react-bootstrap */}
         <Row className="align-items-center">
+          {/* Column component with different breakpoints */}
           <Col xs={12} md={6} xl={7}>
+            {/* Span element with className "tagline" */}
             <span className="tagline">Welcome to my Portfolio!!</span>
+            {/* Heading element with dynamic content */}
             <h1>
               {`Hi I'm webdecoded`}
+              {/* Span element with className "wrap" */}
               <span className="wrap">Web developer!!</span>
             </h1>
+            {/* Paragraph element with text content */}
             <p>
               Welcome to my React developer portfolio! I'm passionate about
               crafting engaging user experiences and building scalable web
@@ -21,12 +36,16 @@ export const Banner = () => {
               efficient, and intuitive interfaces. Let's collaborate and bring
               your ideas to life!
             </p>
+            {/* Button element with onClick event handler */}
             <button onClick={() => console.log("connect")}>
               Let's connect
             </button>
           </Col>
+          {/* Column component with different breakpoints */}
           <Col xs={12} md={6} xl={5}>
+            {/* Image element with src and alt attributes */}
             <img src={headerImg} alt="Header Image" />
+            {/* Anchor element with ArrowRightCircle icon */}
             <a href="#">
               <ArrowRightCircle />
             </a>
