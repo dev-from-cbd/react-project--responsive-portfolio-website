f74f8a6a01d5702cbce3fb8ac3a765d443a49f66
dev-from-cbd <veryfatwombat@gmail.com>
2024-03-12 07:27:34 +1030

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -25,7 +25,7 @@ export const Banner = () => {
     }, delta);
 
     return () => {
-      clearInterval(ticker); // Clearing the interval on unmount
+      clearInterval(ticker);
     };
   }, [text]);
 
@@ -34,25 +34,16 @@ export const Banner = () => {
     let fullText = toRotate[1];
   };
 
-  // Returning JSX for the Banner component
   return (
-    // Section element with className "banner" and id "home"
     <section className="banner" id="home">
-      {/* Container component from react-bootstrap */}
       <Container>
-        {/* Row component from react-bootstrap */}
         <Row className="align-items-center">
-          {/* Column component with different breakpoints */}
           <Col xs={12} md={6} xl={7}>
-            {/* Span element with className "tagline" */}
             <span className="tagline">Welcome to my Portfolio!!</span>
-            {/* Heading element with dynamic content */}
             <h1>
               {`Hi I'm webdecoded`}
-              {/* Span element with className "wrap" */}
               <span className="wrap">Web developer!!</span>
             </h1>
-            {/* Paragraph element with text content */}
             <p>
               Welcome to my React developer portfolio! I'm passionate about
               crafting engaging user experiences and building scalable web
@@ -61,16 +52,12 @@ export const Banner = () => {
               efficient, and intuitive interfaces. Let's collaborate and bring
               your ideas to life!
             </p>
-            {/* Button element with onClick event handler */}
             <button onClick={() => console.log("connect")}>
               Let's connect
             </button>
           </Col>
-          {/* Column component with different breakpoints */}
           <Col xs={12} md={6} xl={5}>
-            {/* Image element with src and alt attributes */}
             <img src={headerImg} alt="Header Image" />
-            {/* Anchor element with ArrowRightCircle icon */}
             <a href="#">
               <ArrowRightCircle />
             </a>
