53fcad228fc61a4c1ea84505b7330d234e0e0555
dev-from-cbd <veryfatwombat@gmail.com>
2024-03-13 07:18:37 +1100

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -31,7 +31,25 @@ export const Banner = () => {
 
   const tick = () => {
     let i = loopNum % toRotate.length;
-    let fullText = toRotate[1];
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
   };
 
   return (
@@ -42,7 +60,7 @@ export const Banner = () => {
             <span className="tagline">Welcome to my Portfolio!!</span>
             <h1>
               {`Hi I'm webdecoded`}
-              <span className="wrap">Web developer!!</span>
+              <span className="wrap">{text}</span>
             </h1>
             <p>
               Welcome to my React developer portfolio! I'm passionate about
