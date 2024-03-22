3522a30af8811403fda40e333ebfdc16b3991bcd
dev-from-cbd <veryfatwombat@gmail.com>
2024-03-23 08:44:08 +1030

    React JS component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -30,7 +30,9 @@ export const Skills = () => {
   return (
     <section className="skill" id="skills">
       <Container>
-        <Row>Row</Row>
+        <Row>
+          <Col>Col</Col>
+        </Row>
       </Container>
     </section>
   );
