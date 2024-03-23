7f2f989db038d724766e46c6d03ec38c493b3b0b
dev-from-cbd <veryfatwombat@gmail.com>
2024-03-24 08:39:41 +1030

    component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,5 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 //import meter1 from "../assets/img/meter1.svg";
 //import meter2 from "../assets/img/meter2.svg";
 //import meter3 from "../assets/img/meter3.svg";
