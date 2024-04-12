585cbfa4e0b438664ffc55b09a886dfefb069b5d
dev-from-cbd <veryfatwombat@gmail.com>
2024-04-13 09:28:13 +0930

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
-import { Container } from "react-bootstrap";
-import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Row className="align-item-center">
-          <MailchimpSubscribe />
-          <Col sm={6}>Col</Col>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
         </Row>
       </Container>
     </footer>
